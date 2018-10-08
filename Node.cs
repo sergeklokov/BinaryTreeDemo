25eb5b4ccfa27fbaa4f6ed9646abf90cfad3dce0
sergeklokov <serge.klokov@gmail.com>
2018-10-07 23:07:29 -0700

    Initial commit

---
+++ Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeDemo
+{
+    class Node<T>
+    {
+        private T data;
+        private NodeList<T> neighbors = null;
+
+        public Node() { }
+        public Node(T data) : this(data, null) { }
+        public Node(T data, NodeList<T> neighbors)
+        {
+            this.data = data;
+            this.neighbors = neighbors;
+        }
+    }
+}
---
+++ NodeList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeDemo
+{
+    class NodeList<T>: Collection<Node<T>>   //System.Collections.ObjectModel;
+    {
+    }
+}
